6d4bc6c45145b76b2b9d756c9e2d900142543c53
Tran Anh Dung <trandunguet@gmail.com>
2019-02-17 15:05:09 +0700

    extract & label features

--- features_labeling.py
+++ features_labeling.py
@@ -2,58 +2,70 @@
 
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+import sys
+import time
 
 import core
 
 if __name__ == '__main__':
-    right_arrow = 83
-    space = 32
-    escape = 27
-    n_key = 110
-    p_key = 112
+    start_time = time.clock()
 
-    video = core.Video("assets/test.avi")
-    plt.ion()
-    plt.show()
+    # Read label file, mark fighting frames ids
+    markup_file = open("assets/clip1_label.txt", "r")
+    markup_file.readline()
+    fight_frames = []
 
+    while True:
+        line = markup_file.readline().replace('\t', ' ').replace(';', ' ').replace('\n', ' ').split()
+        if len(line) < 3:
+            break
+
+        if line[0][0] == '[':
+            line.pop(0)
+
+        if line[0][0] == '[':
+            line.pop(0)
+
+        if line[2] != 'Fight':
+            continue
+        
+        fight_frames.append((int(line[0]), int(line[1])))
+
+    # Read video, extract features, label features
+    video = core.Video("assets/clip1.wmv")
     output_negative = open("features/negative.txt", "a+")
     output_positive = open("features/positive.txt", "a+")
+    negative_count = positive_count = 0
+
+    def isViolent(id_range):
+        for begin, end in fight_frames:
+            if begin <= id_range[0] < id_range[1] <= end:
+                return True
+        return False
 
     while True:
         sequence = video.next_sequence()
         if not sequence:
-            print("Video ended.")
+            print("\nVideo ended.")
             break
 
-        vif, mean, variance = sequence.get_flows().get_vif()
-        print("vif: ", vif)
-        print("mean: ", mean)
-        print("variance: ", variance, "\n")
-        plt.clf()
-        plt.plot(vif)
-        plt.draw()
-        plt.pause(0.001)
+        vif, _, _ = sequence.get_flows().get_vif()
 
-        key = space
-        while key != right_arrow:
-            if key == space:
-                for frame in sequence.frames:
-                    cv.imshow("video", frame)
-                    cv.waitKey(30)
+        if isViolent(sequence.id_range):
+            output_positive.write(np.array2string(vif) + '\n')
+            positive_count += 1
+        else:
+            output_negative.write(np.array2string(vif) + '\n')
+            negative_count += 1
 
-            if key == escape:
-                exit()
-
-            if key == n_key:
-                output_negative.write(np.array2string(vif) + '\n')
-                break
-
-            if key == p_key:
-                output_positive.write(np.array2string(vif) + '\n')
-                break
-
-            key = cv.waitKey()
+        percentage = int(sequence.id_range[0] * 100 / video.frame_count)
+        sys.stdout.write('\rextracting & labeling features: {} %'.format(percentage))
+        sys.stdout.flush() 
 
     output_negative.close()
     output_positive.close()
+
+    print("extrated total: {} sequences".format(negative_count + positive_count))
+    print("positive: {}".format(positive_count))
+    print("negative: {}".format(negative_count))
+    print("time elapsed: {} seconds".format(time.clock() - start_time))
