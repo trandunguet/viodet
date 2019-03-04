10f6d1ec2a3d125430a507a5a60d7db58573965f
Tran Anh Dung <trandunguet@gmail.com>
2019-03-04 13:49:56 +0700

    add script to get dataset

---
+++ scripts/get_BEHAVE_dataset.sh
@@ -0,0 +1,3 @@
+mkdir -p dataset/BEHAVE
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/markup.txt --directory-prefix=dataset/BEHAVE/
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_margaret_1_24_01_2007.wmv --directory-prefix=dataset/BEHAVE/
