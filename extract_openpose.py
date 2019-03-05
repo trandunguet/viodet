cb3d103dc8eb14c30e035e3442afc86cbfd93394
Tran Anh Dung <trandunguet@gmail.com>
2019-03-05 23:22:38 +0700

    test openpose on BEHAVE

---
+++ extract_openpose.py
@@ -0,0 +1,38 @@
+import time
+
+from openpose import pyopenpose as op
+import cv2 as cv
+
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('dataset/BEHAVE/output.avi',fourcc, 20.0, (640,480))
+
+cap = cv.VideoCapture("dataset/BEHAVE/fight_margaret_1_24_01_2007.wmv")
+cap.set(cv.CAP_PROP_POS_FRAMES, 60683)
+
+params = dict()
+params["model_folder"] = "models/openpose/"
+
+# Starting OpenPose
+opWrapper = op.WrapperPython()
+opWrapper.configure(params)
+opWrapper.start()
+datum = op.Datum()
+
+t = time.clock()
+# Process Image
+while True:
+    print('{} fps'.format(1 / (time.clock() - t)))
+    t = time.clock()
+
+    _, imageToProcess = cap.read()
+    datum.cvInputData = imageToProcess
+    opWrapper.emplaceAndPop([datum])
+
+    # Display Image
+    print("Body keypoints: \n" + str(datum.poseKeypoints))
+    cv.imshow("OpenPose", datum.cvOutputData)
+    out.write(datum.cvOutputData)
+    if cv.waitKey(1) == 27:
+        break
+
+out.release()
---
+++ scripts/get_openpose_models.sh
@@ -0,0 +1,3 @@
+mkdir -p models/openpose/pose/body_25/
+wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/body_25/pose_iter_584000.caffemodel -O models/openpose/pose/body_25/pose_iter_584000.caffemodel
+wget https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/pose/body_25/pose_deploy.prototxt -O models/openpose/pose/body_25/pose_deploy.prototxt
