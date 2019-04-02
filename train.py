9b104fd76ea3f821ea33081d3eed564985f1a2e5
Tran Anh Dung <trandunguet@gmail.com>
2019-04-02 15:14:36 +0700

    add cli argument to train.py

--- train.py
+++ train.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
+import sys
 
 from sklearn import datasets, svm, metrics
 import numpy as np
 
 import core
 
-data, target = core.load_features("features")
-n_samples = len(data)
-alpha = 1 / 2
+if __name__ == '__main__':
+      if len(sys.argv) != 2:
+            print("Invalid argument!")
+            print("Usage: {} FEATURES_FOLDER_PATH".format(sys.argv[0]))
+            exit()
 
-print("loaded {} samples".format(n_samples))
+      data, target = core.load_features(sys.argv[1])
+      n_samples = len(data)
+      alpha = 1 / 2
 
-print("data shape: {} \ntarget shape: {}".format(data.shape, target.shape))
+      print("loaded {} samples".format(n_samples))
 
-# Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma='auto')
+      print("data shape: {} \ntarget shape: {}".format(data.shape, target.shape))
 
-print("training")
+      # Create a classifier: a support vector classifier
+      classifier = svm.SVC(gamma='auto')
 
-# We learn the digits on the first half of the digits
-classifier.fit(data[:int(n_samples * alpha)], target[:int(n_samples * alpha)])
+      print("training")
 
-print("predicting")
+      # We learn the digits on the first half of the digits
+      classifier.fit(data[:int(n_samples * alpha)], target[:int(n_samples * alpha)])
 
-# Now predict the value of the digit on the second half:
-expected = target[int(n_samples * (1 - alpha)):]
-predicted = classifier.predict(data[int(n_samples * (1 - alpha)):])
+      print("predicting")
 
-print("Classification report for classifier %s:\n%s\n"
-      % (classifier, metrics.classification_report(expected, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+      # Now predict the value of the digit on the second half:
+      expected = target[int(n_samples * (1 - alpha)):]
+      predicted = classifier.predict(data[int(n_samples * (1 - alpha)):])
+
+      print("Classification report for classifier %s:\n%s\n"
+            % (classifier, metrics.classification_report(expected, predicted)))
+      print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
