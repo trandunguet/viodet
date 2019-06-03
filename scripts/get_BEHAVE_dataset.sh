fdf0f1e5e1acdc3dd86c86010ef7c3d82abb20ac
Tran Anh Dung <trandunguet@gmail.com>
2019-06-03 22:50:05 +0700

    update script to get dataset

--- scripts/get_BEHAVE_dataset.sh
+++ scripts/get_BEHAVE_dataset.sh
@@ -1,3 +1,15 @@
-mkdir -p BEHAVE/data
-wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/markup.txt --directory-prefix=BEHAVE/data
-wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_margaret_1_24_01_2007.wmv --directory-prefix=BEHAVE/data
+mkdir -p BEHAVE/clip_1/data
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_margaret_1_24_01_2007.wmv -O BEHAVE/clip_1/data/clip_1.wmv
+wget 'https://docs.google.com/uc?export=download&id=1lEvgaRieoRI9RYZCulKWElxRJ8BtFW_Z' -O BEHAVE/clip_1/data/clip_1.txt
+
+mkdir -p BEHAVE/clip_2/data
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_margaret_2_24_01_2007.wmv -O BEHAVE/clip_2/data/clip_2.wmv
+wget 'https://docs.google.com/uc?export=download&id=15xlrTLUG0ZntdNrmr3-BKYppNd3Ezirv' -O BEHAVE/clip_2/data/clip_2.txt
+
+mkdir -p BEHAVE/clip_3/data
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_taku_1_24_01_2007.wmv -O BEHAVE/clip_3/data/clip_3.wmv
+wget 'https://docs.google.com/uc?export=download&id=1BS42Zyg1pF0Wdg5Wu7ZiwapBSwn3cqax' -O BEHAVE/clip_3/data/clip_3.txt
+
+mkdir -p BEHAVE/clip_4/data
+wget http://groups.inf.ed.ac.uk/vision/BEHAVEDATA/INTERACTIONS/fight_taku_2_24_01_2007.wmv -O BEHAVE/clip_4/data/clip_4.wmv
+wget 'https://docs.google.com/uc?export=download&id=1kRdgLuxUlARBVp_bU0wUsvhElfTUlVj4' -O BEHAVE/clip_4/data/clip_4.txt
